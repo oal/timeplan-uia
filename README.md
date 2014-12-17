01bee58859215587226731bbd2e8c86180a7d3d4
Olav Lindekleiv <olav@lindekleiv.com>
2014-12-17 18:37:39 +0100

    Første versjon.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Timeplaner for Universitetet i Agder
+Dette er et program for å generere .csv-filer fra timeplansystemet til UiA. Disse filene kan så importeres direkte til Google Calendar, og synkroniseres mot din smarttelefon eller PC.
+
+Ferdig genererte filer ligger i "timeplaner"-mappen.
+
+### Hvordan bruker jeg timeplanen i Google Docs?
+Se [denne](https://support.google.com/calendar/answer/37118?hl=en) siden. Kort forklart oppretter du en kalender, for så å trykke "Andre kalendere" og "Importer kalender". Her velger du kalenderen du ønsker at timeplanen skal legges til i, og velger .csv-filen herfra.
+
+### Hvordan kan jeg generere nye versjoner av timeplanene?
+Installer [Go](http://golang.org/doc/install), og kjør "go get github.com/oal/timeplan-uia", og kjør så "timeplan-uia" fra din `$GOPATH/bin`.
+
+Alternativt kan du klone denne repositoryen, og kjøre `go run timeplan.go` fra denne mappen.
+
+### Hvorfor trenger jeg dette?
+Jeg lagde dette programmet for å spare litt tid selv med å legge inn min kalender på dataingeniørstudiet, men det var såpass lite ekstra arbeid å utvide det til andre linjer også, så hvorfor ikke. Frem til UiA offisielt får støtte for iCal/CSV, vil dette verktøyet forhåpentligvis være nyttig for noen.
---
+++ timeplan.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+var client *http.Client
+
+const URL = "http://timeplan.uia.no/swsuiav/public/no/default.aspx"
+const FOLDER = "timeplaner/v2015"
+
+type Department struct {
+	Name string
+	Code string
+}
+
+var departments = []*Department{}
+
+func main() {
+	os.MkdirAll(FOLDER, 0777)
+
+	jar, _ := cookiejar.New(nil)
+	client = &http.Client{
+		Jar: jar,
+	}
+
+	doc, err := loadDepartments()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, dep := range departments {
+		err = loadTimetable(doc, dep)
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(1 * time.Second) // Give the server some room to breathe
+	}
+}
+
+func loadDepartments() (*goquery.Document, error) {
+	resp, err := client.Get(URL)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Find("#dlObject option").Each(func(i int, sel *goquery.Selection) {
+		val, _ := sel.Attr("value")
+		departments = append(departments, &Department{sel.Text(), val})
+	})
+
+	return doc, nil
+}
+
+func loadTimetable(doc *goquery.Document, department *Department) error {
+	viewstate, _ := doc.Find("#__VIEWSTATE").Attr("value")
+	viewstateGen, _ := doc.Find("#__VIEWSTATEGENERATOR").Attr("value")
+	eventValidation, _ := doc.Find("#__EVENTVALIDATION").Attr("value")
+	linkType, _ := doc.Find("#tLinkType").Attr("value")
+	lbWeeks, _ := doc.Find("#lbWeeks option").Eq(1).Attr("value")
+	lbDays, _ := doc.Find("#lbDays option").Eq(0).Attr("value")
+	radioType, _ := doc.Find("#RadioType_0").Attr("value")
+
+	data := url.Values{
+		"__VIEWSTATE":          []string{viewstate},
+		"__VIEWSTATEGENERATOR": []string{viewstateGen},
+		"__EVENTVALIDATION":    []string{eventValidation},
+		"tLinkType":            []string{linkType},
+		"dlObject":             []string{department.Code},
+		"lbWeeks":              []string{lbWeeks},
+		"lbDays":               []string{lbDays},
+		"RadioType":            []string{radioType},
+		"bGetTimetable":        []string{"Vis+timeplan"},
+		"tWildcard":            []string{""},
+		"__EVENTTARGET":        []string{""},
+		"__EVENTARGUMENT":      []string{""},
+		"__LASTFOCUS":          []string{""},
+	}
+
+	resp, err := client.PostForm(URL, data)
+	if err != nil {
+		return err
+	}
+
+	generateCSV(resp.Body)
+	return nil
+}
+
+func generateCSV(r io.Reader) error {
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return err
+	}
+
+	lines := []string{"Subject,Start Date,Start Time,End Date,End Time,Description,Location"}
+
+	rows := doc.Find("tr.tr2")
+	rows.Each(func(i int, s *goquery.Selection) {
+		cells := s.Find("td")
+
+		dateString := ""
+		var from time.Time
+		var to time.Time
+		var subject string
+		var location string
+		var description string
+
+		cells.Each(func(j int, s *goquery.Selection) {
+			switch j {
+			case 1:
+				dateString = s.Text() + " 2015"
+			case 2:
+				times := strings.Split(strings.TrimSpace(s.Text()), "-")
+				from, _ = time.Parse("02 Jan 2006 15.04", dateString+" "+times[0])
+				to, _ = time.Parse("02 Jan 2006 15.04", dateString+" "+times[1])
+			case 3:
+				subject = s.Text()
+			case 4:
+				location = s.Text()
+			case 5:
+				description = s.Text()
+			}
+		})
+
+		line := []string{
+			subject,
+			from.Format("2006-01-02"),
+			from.Format("15:04"),
+			to.Format("2006-01-02"),
+			to.Format("15:04"),
+			description,
+			location,
+		}
+
+		lines = append(lines, strings.Join(line, ","))
+	})
+
+	if len(lines) < 5 {
+		return nil
+	}
+
+	title := doc.Find("p.title i").Text()
+	csv := []byte(strings.Join(lines, "\r\n"))
+	err = ioutil.WriteFile(FOLDER+"/"+title+".csv", csv, 0664)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Created timetable for", title)
+	return nil
+}
