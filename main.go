f11f60d557799fa314a8c4a934d5052882658edb
Olav Lindekleiv <olav@lindekleiv.com>
2015-08-17 20:48:35 +0200

    Added --url flag to allow passing in a single timetable URL and export a .csv just for that URL.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 var serve = flag.Bool("serve", false, "Start server")
 var update = flag.Bool("update", false, "Update time tables")
+var url = flag.String("url", "", "Download a single calendar based on a URL")
 
 func main() {
 	flag.Parse()
@@ -16,5 +17,7 @@ func main() {
 		startServer()
 	} else if *update {
 		utils.UpdateTimetables()
+	} else if *url != "" {
+		utils.UpdateSingleURL(*url)
 	}
 }
